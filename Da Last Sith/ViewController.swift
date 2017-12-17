34d5741d72f2b99e484cdc2bf9835285fdd9c3d8
Tyler Cliber <tylercliber@Tylers-Mac.local>
2017-12-17 13:11:03 -0500

    Kinda broken but works.

--- Da Last Sith/ViewController.swift
+++ Da Last Sith/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    @IBOutlet private var Rey1: UIButton!
+    @IBOutlet private var Rey2: UIButton!
+    
+    @IBOutlet private var Kylo1: UIButton!
+    @IBOutlet private var Kylo2: UIButton!
+    
+    
+    @IBOutlet private var myLabel: UILabel!
+    @IBAction func display(sender: UIButton) {
+        switch sender {
+        case Rey1:
+            // Rey chooses the light
+            myLabel.text = ("Rey chose the light side")
+        case Rey2:
+            // Rey chooses the dark
+            myLabel.text = ("Rey chose the dark side")
+        case Kylo1:
+            // Kylo chooses the light
+            myLabel.text = ("Kylo chose the light side")
+        case Kylo2:
+            // Kylo chooses the dark
+            myLabel.text = ("Kylo chose the dark side")
+        default:
+            break
+            
+        }
+        
+    }
+    @IBOutlet private var myImages: UIImageView!
+    @IBAction func displayImage(sender: UIButton) {
+        switch sender {
+        case Rey1:
+            // Rey chooses the light
+            let lightSide: UIImage = UIImage(named: "BlueLightSaber")!
+            myImages.image = lightSide
+        case Rey2:
+            // Rey chooses the dark
+            let darkSide: UIImage = UIImage(named: "RedLightSaber")!
+            myImages.image = darkSide
+        case Kylo1:
+            // Kylo chooses the light
+            let lightSide: UIImage = UIImage(named: "BlueLightSaber")!
+            myImages.image = lightSide
+        case Kylo2:
+            // Kylo chooses the dark
+            let darkSide: UIImage = UIImage(named: "RedLightSaber")!
+            myImages.image = darkSide
+        default:
+            break
+            
+        }
+        
+    }}
 
